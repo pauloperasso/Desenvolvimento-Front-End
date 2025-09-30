8383b4beaefdbe5f350bd714efb093307d5ea4f0
Paulo Perasso <paulomonreale@gmail.com>
2025-09-30 11:07:56 -0300

    Commits Oitava Aula Utilizando HTML e CSS
    Iniciamos o desenvolvimento back-end com JavaScript explorando suas funções e possibilidades que a linguagem oferece.

---
+++ variaveis.js
@@ -0,0 +1,33 @@
+const nome = 'Paulo'
+const valor = 37
+const senha = '123asd'
+
+console.log(nome)
+console.log(valor)
+console.log(senha)
+
+var x = 3
+var y = 5
+
+console.log(x, !y)
+
+//Comparando Var com Let
+var m
+let n
+
+console.log("m = " + m + ", n = " + n)
+
+var idade = 20
+if (idade > 18)
+    nome = "Astolfo"
+console.log(nome + "pode Dirigir corrteamente.")
+
+let pessoa ="Adalto"
+if (idade > 25)
+    console.log("Pode dirigir!")
+
+//Coerção
+const n1 = 2
+const n2 = '34'
+const n3 = n1 + n2
+console.log("n3 = " + n3)
---
+++ vetores.js
@@ -0,0 +1,16 @@
+// Declaração
+v1 = []
+v1[0] = 2
+v1[10] = 7.5
+v1[3] = "abc"
+
+console.log(v1)
+console.log("Tamanho de V1" + v1.lenght)
+console.table(v1)
+
+v2 = [10, "abc", 7.5]
+
+console.log(v2)
+console.log("Tamanho de V2" + v2.lenght)
+console.table(v2)
+
